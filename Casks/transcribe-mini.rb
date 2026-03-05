556c2ba96c210b8526f9d22018cd2809bacb0860
Eugene Beloded <belodedevgenij@gmail.com>
2026-03-05 12:43:25 -0800

    transcribe-mini: use versioned url template

--- Casks/transcribe-mini.rb
+++ Casks/transcribe-mini.rb
@@ -2,7 +2,7 @@ cask "transcribe-mini" do
   version "0.1.0"
   sha256 "4df2df3430412564755d7036f20cea9495357578ccbda0cc8cd7f93906d28f61"
 
-  url "https://github.com/ebeloded/transcribe/releases/download/v0.1.0/TranscribeMini.zip"
+  url "https://github.com/ebeloded/transcribe/releases/download/v#{version}/TranscribeMini.zip"
   name "TranscribeMini"
   desc "Hold-to-talk menubar transcription app for macOS"
   homepage "https://github.com/ebeloded/transcribe"
